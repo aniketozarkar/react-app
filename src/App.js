f7096c101e0282c9d2e8111c712d80789ad7f3a9
Aniket Ozarkar <ozarkaraniket@gmail.com>
2020-01-27 00:14:09 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name:'aniket', age:22},
+      {name:'naru', age:22},
+      {name:'manali',age:22}
+    ]
+  };
+
   render() {
     return (
-          <div className="App">
-              <h1>Handsome boiz</h1>
-              <Person name="Aniket" age="22"/>
-              <Person name="Kapil" age="22"/>
-              <Person name="Swapnaja" age="22"/>  
-          </div>
+      <div className="App">
+        <h1>Handsome boiz</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
       );
-    //   return React.createElement('div', {className:'App'}, React.createElement('h1',null, 'Hi Heisenberg'));
+    //return React.createElement('div', {className:'App'}, React.createElement('h1',null, 'Hi Heisenberg'));
   }
 }
 
